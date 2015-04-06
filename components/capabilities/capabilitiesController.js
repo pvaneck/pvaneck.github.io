3855e6ee9b2fadd3f8fffdd6f85dfcac5e8bee52
Paul Van Eck <pvaneck@us.ibm.com>
2015-04-06 12:43:03 -0700

    Added local copy of capabilities file for now until I figure out how to increase the api rate limiting.

--- components/capabilities/capabilitiesController.js
+++ components/capabilities/capabilitiesController.js
@@ -7,7 +7,9 @@ var refstackApp = angular.module('refstackApp');
 refstackApp.controller('capabilitiesController', ['$scope', '$http', function($scope, $http) {
     $scope.version = "2015.03";
     $scope.update = function() {
-        var content_url = 'https://api.github.com/repos/openstack/defcore/contents/'.concat($scope.version, '.json');
+        //Rate-limiting is an issue right now with this url. Using a local copy instead.
+        //var content_url = 'https://api.github.com/repos/openstack/defcore/contents/'.concat($scope.version, '.json');
+        var content_url = 'assets/capabilities/'.concat($scope.version, '.json');
         $http.get(content_url).success(function(data) {
             $scope.data = data;
             $scope.capabilities = JSON.parse(atob($scope.data.content.replace(/\s/g, '')));
